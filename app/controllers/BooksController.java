80da84662b60cff9caf09bb03b58b5a4075372ee
HoJ0 <joffeehhl@gmail.com>
2018-04-25 14:14:16 -0400

    Implement Save Method Of BookStore Application

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -1,13 +1,19 @@
 package controllers;
 
 import models.Book;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.Set;
 import views.html.books.*;
+import javax.inject.Inject;
 
 public class BooksController extends Controller {
+
+    @Inject
+    FormFactory formFactory;
 
     // for all books
     public Result index(){
@@ -17,12 +23,16 @@ public class BooksController extends Controller {
 
     // to create book
     public Result create(){
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class);
+        return ok(create.render(bookForm));
     }
 
     // to save book
     public Result save(){
-        return TODO;
+        Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
+        Book book = bookForm.get();
+        Book.add(book);
+        return redirect(routes.BooksController.index());
     }
     public Result edit(Integer id){
         return TODO;
